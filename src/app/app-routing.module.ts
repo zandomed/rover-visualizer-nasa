62f3a706adf0bc6568fbac55ebdb095a273854d4
Miguel Mendoza <miguel@zandome.com>
2021-11-04 23:56:42 -0400

    [feat] Implement base modules
    - Core
    - Gallery
    - PageNotFound
    - Shared

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,22 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
   },
+  {
+    path: 'gallery',
+    loadChildren: () =>
+      import('./gallery/gallery.module').then((m) => m.GalleryModule),
+  },
+  {
+    path: '**',
+    loadChildren: () =>
+      import('./page-not-found/page-not-found.module').then(
+        (m) => m.PageNotFoundModule
+      ),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
